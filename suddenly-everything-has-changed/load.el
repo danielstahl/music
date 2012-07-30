f636383457687d07190b34bea2b1ef25717035af
danielstahl <oe.daniel.stahl@gmail.com>
2012-07-30 23:04:07 +0200

    Fixed load errors

--- suddenly-everything-has-changed/load.el
+++ suddenly-everything-has-changed/load.el
@@ -1,9 +1,12 @@
 (sclang-start)
-(sclang-eval-string 
- "this.executeFile(\"/Users/daniel_stahl/Documents/projects/music/piece/Options.scd\")" 't)
+
+(let ((dir (file-name-directory (or load-file-name buffer-file-name))))
+  (sclang-eval-string (format "this.executeFile(%S)" (format "%s%s.scd" dir "Options")) 't))
+
 (sclang-server-boot)
 
-(let ((dir "/Users/daniel_stahl/Documents/projects/music/piece/"))
+(let ((dir (file-name-directory (or load-file-name buffer-file-name))))
     (dolist (file '("Instruments" "Common" "Harmony" "Rythm"))
       (sclang-eval-string (format "this.executeFile(%S)" (format "%s%s.scd" dir file)) 't)))
+
 
